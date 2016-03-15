aa5a6cbc382016c5eb8ef41f5b212e82ddd9360b
Elena Gancheva <eagan4eva@gmail.com>
2016-03-15 21:04:32 +0000

    small bug fixed

--- Resume Details/app/components/shared/config.js
+++ Resume Details/app/components/shared/config.js
@@ -2,7 +2,7 @@
 var applicationSettingsModule = require('application-settings');
 
 var configObject = {
-    requestUrl: "https://www.googleapis.com/books/v1/users/105745440028303228683/bookshelves/0/volumes?key=AIzaSyD-Bd2cD8QIZNkRTd2hx6saE9-OM4ATusQ";
+    requestUrl: "https://www.googleapis.com/books/v1/users/105745440028303228683/bookshelves/0/volumes?key=AIzaSyD-Bd2cD8QIZNkRTd2hx6saE9-OM4ATusQ"
 }
 
-exports.configObject = configObject;
+module.exports = configObject;
